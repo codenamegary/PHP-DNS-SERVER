8d21a2827c7138f8dec6248211172351d548cd50
Gary Saunders <gary@powrit.com>
2014-11-29 17:56:28 -0500

    Composer updated to use some Symfony process components to launch a server during testing.

---
+++ src/QuestionPacker.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Class Packer
+ * @package yswery\DNS
+ *
+ * Takes data and packs it into a valid DNS query for writing to a socket.
+ */
+
+namespace yswery\DNS;
+
+class QuestionPacker {
+
+
+
+}
